24c8cde1e321af04780c3bbcc8608f34c4c7f29d
LuanBH <luaninu123@gmail.com>
2024-06-17 23:43:52 +0700

    complete exercise module 1 - week 2

---
+++ module_1/week_2/Ex3.py
@@ -0,0 +1,24 @@
+# Testing variables
+test_file_path = 'module_1\week_2\P1_data.txt'
+
+
+# Define function
+def count_word(file_path):
+    # read and reprocessing text file
+    file = open(file_path, "r")
+    text_data = file.read().lower().replace("\n", " ")
+    file.close()
+
+    # split into words_array
+    word_arr = text_data.split(" ")
+    res = {}
+    for word in word_arr:
+        if res.get(word):
+            res[word] += 1
+        else:
+            res[word] = 1
+    print(res)
+
+
+# Call function
+count_word(test_file_path)
